bc9b84234638838f26935da916772781c8b93275
Jakub Charyton <mipus123@gmail.com>
2017-11-10 15:14:02 +0100

    Models creation for rango apllication

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length = 128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def days_from_recent_edition(self):
+        return timezone.now() - self.date
