51717291cf5696ff39f80be877340144d42b0ccd
Jakub Charyton <jakub.charyton@gmail.com>
2017-11-13 20:49:50 +0100

    Migration function for rango application database

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length = 128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
