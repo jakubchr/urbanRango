22a99e436a64fc60132725b373c79fb3ed73255f
Jakub Charyton <mipus123@gmail.com>
2018-02-13 13:33:24 +0100

    Initial changes for user authentication. Fix for file upload content types

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length = 128, unique=True)
@@ -33,3 +34,12 @@ class Page(models.Model):
 
     def days_from_recent_edition(self):
         return timezone.now() - self.date
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
--- tango_with_django_project/rango/templatetags/rango_template_tags.py
+++ tango_with_django_project/rango/templatetags/rango_template_tags.py
@@ -3,6 +3,10 @@ from rango.models import Category
 
 register = template.Library()
 
-@register.inclusion_tag('rango/cats.html')
-def get_category_list():
-    return {'cats': Category.objects.all()}
+@register.inclusion_tag('rango/template_sites/top_cats.html')
+def get_top_category_list():
+    return {'cats': Category.objects.order_by('-likes')[:5]}
+
+@register.inclusion_tag('rango/template_sites/latest_cats.html')
+def get_latest_category_list():
+    return {'cats': Category.objects.order_by('date')[:5]}
